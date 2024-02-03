063a466f7ed7ef463a86dc26ecfdfd072953558e
Andrey Guschin <andrey67895@gmail.com>
2024-02-04 03:31:55 +0500

    fix

--- src/main/java/ru/account/api/model/AccountModel.java
+++ src/main/java/ru/account/api/model/AccountModel.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Assertions;
+import ru.account.db.Account;
 
 import java.util.UUID;
 
@@ -16,4 +19,19 @@ public class AccountModel {
     private String firstName;
     private String lastName;
     private UUID passportUUID;
+
+    public static AccountModel getAccountModel() {
+        return AccountModel.builder()
+                .firstName(RandomStringUtils.random(12, true, false))
+                .lastName(RandomStringUtils.random(8, true, false))
+                .passportUUID(UUID.randomUUID()).build();
+    }
+
+    public void compareTo(Account account) {
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(this.getPassportUUID().toString(), account.getPassportUUID().toString()),
+                () -> Assertions.assertEquals(this.getFirstName(), account.getFirstName()),
+                () -> Assertions.assertEquals(this.getLastName(), account.getLastName())
+        );
+    }
 }
--- src/test/java/ru/account/AccountTest.java
+++ src/test/java/ru/account/AccountTest.java
@@ -1,29 +1,20 @@
 package ru.account;
 
 import io.restassured.response.Response;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.util.StringUtils;
-import ru.account.api.controller.AccountsApiService;
 import ru.account.api.model.AccountModel;
 import ru.account.db.Account;
-import ru.account.db.AccountsDBService;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 
 
-@Execution(ExecutionMode.SAME_THREAD)
 public class AccountTest extends BaseClass{
 
 
@@ -38,7 +29,7 @@ public class AccountTest extends BaseClass{
     void getFinish() {
         long finish = System.currentTimeMillis();
         System.out.println("Время выполнения: " + (finish - start));
-        accountsDBService.saveTime(String.valueOf((finish-start)), 2);
+        accountsDBService.saveTime(String.valueOf((finish - start)), 10);
     }
 
 
@@ -46,18 +37,11 @@ public class AccountTest extends BaseClass{
     @Execution(ExecutionMode.CONCURRENT)
     @RepeatedTest(1000)
     void test() {
-        AccountModel accountModel = AccountModel.builder()
-                .firstName(RandomStringUtils.random(12, true, false))
-                .lastName(RandomStringUtils.random(8, true, false))
-                .passportUUID(UUID.randomUUID()).build();
+        AccountModel accountModel = AccountModel.getAccountModel();
         Response response = accountsApiService.post(accountModel);
         Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatusCode());
         Optional<Account> optionalAccount = accountsDBService.getAccountsByPassportUUID(accountModel.getPassportUUID());
         Assertions.assertTrue(optionalAccount.isPresent());
-        Assertions.assertAll(
-                () -> Assertions.assertEquals(accountModel.getPassportUUID().toString(), optionalAccount.get().getPassportUUID().toString()),
-                () -> Assertions.assertEquals(accountModel.getFirstName(), optionalAccount.get().getFirstName()),
-                () -> Assertions.assertEquals(accountModel.getLastName(), optionalAccount.get().getLastName())
-        );
+        accountModel.compareTo(optionalAccount.get());
     }
 }
